74f659dee59d07741f5723662faafc91ff477238
Piker215 <kielbasa7648@gmail.com>
2026-04-16 17:51:08 +0200

    Fixed movement bugs

--- playerreal.gd
+++ playerreal.gd
@@ -2,10 +2,6 @@ extends CharacterBody2D
 const speed = 120.0
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	var directionx := Input.get_axis("ui_left", "ui_right")
-	var directiony := Input.get_axis("ui_down", "ui_up")
-	if directionx:
-		velocity.x = speed * directionx
-	elif directiony:
-		velocity.y = speed * directiony
+	var movement = Input.get_vector("ui_left", "ui_right", "ui_down", "ui_up")
+	velocity = speed * movement
 	move_and_slide()
---
+++ scenes/playerreal.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+var speed := 100
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	var direction = Input.get_vector("Left", "Right", "Up", "Down")
+	velocity = speed * direction
+	move_and_slide()
+	pass
